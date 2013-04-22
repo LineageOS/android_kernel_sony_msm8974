92236c793109c29d7c3524bf299c56d5d15cb892
Joseph Lo <josephl@nvidia.com>
2013-04-22 14:39:18 +0800

    CHROMIUM: sched: update the average of nr_running
    Doing a Exponential moving average per nr_running++/-- does not
    guarantee a fixed sample rate which induces errors if there are lots of
    threads being enqueued/dequeued from the rq (Linpack mt). Instead of
    keeping track of the avg, the scheduler now keeps track of the integral
    of nr_running and allows the readers to perform filtering on top.
    
    Original-author: Sai Charan Gurrappadi <sgurrappadi@nvidia.com>
    
    BUG=chrome-os-partner:42129
    TEST=Boot jerry
    
    Signed-off-by: Joseph Lo <josephl@nvidia.com>
    Signed-off-by: Andrew Bresticker <abrestic@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/174694
    (cherry picked from commit 63853a1603544d1e6c3f8b63d44fa5a27cafa0aa)
    
    Conflicts:
    	kernel/sched/core.c
    
    Change-Id: I8d2bb4b9d3f650eded68ecd0f1bc2547185c0c0b
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -2197,12 +2197,42 @@ unsigned long nr_iowait_cpu(int cpu)
 	return atomic_read(&this->nr_iowait);
 }
 
+
 unsigned long this_cpu_load(void)
 {
 	struct rq *this = this_rq();
 	return this->cpu_load[0];
 }
 
+#ifdef CONFIG_CPU_QUIET
+u64 nr_running_integral(unsigned int cpu)
+{
+	unsigned int seqcnt;
+	u64 integral;
+	struct rq *q;
+
+	if (cpu >= nr_cpu_ids)
+		return 0;
+
+	q = cpu_rq(cpu);
+
+	/*
+	 * Update average to avoid reading stalled value if there were
+	 * no run-queue changes for a long time. On the other hand if
+	 * the changes are happening right now, just read current value
+	 * directly.
+	 */
+
+	seqcnt = read_seqcount_begin(&q->ave_seqcnt);
+	integral = do_nr_running_integral(q);
+	if (read_seqcount_retry(&q->ave_seqcnt, seqcnt)) {
+		read_seqcount_begin(&q->ave_seqcnt);
+		integral = q->nr_running_integral;
+	}
+
+	return integral;
+}
+#endif
 
 /* Variables and functions for calc_load */
 static atomic_long_t calc_load_tasks;
--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -365,6 +365,13 @@ struct rq {
 #endif
 	int skip_clock_update;
 
+#ifdef CONFIG_CPU_QUIET
+	/* time-based average load */
+	u64 nr_last_stamp;
+	u64 nr_running_integral;
+	seqcount_t ave_seqcnt;
+#endif
+
 	/* capture load from *all* tasks on this cpu: */
 	struct load_weight load;
 	unsigned long nr_load_updates;
@@ -922,17 +929,61 @@ extern void cpuacct_charge(struct task_struct *tsk, u64 cputime);
 static inline void cpuacct_charge(struct task_struct *tsk, u64 cputime) {}
 #endif
 
-static inline void inc_nr_running(struct rq *rq)
+static inline void __inc_nr_running(struct rq *rq)
 {
 	sched_update_nr_prod(cpu_of(rq), rq->nr_running, true);
 	rq->nr_running++;
 }
 
-static inline void dec_nr_running(struct rq *rq)
+static inline void __dec_nr_running(struct rq *rq)
 {
 	sched_update_nr_prod(cpu_of(rq), rq->nr_running, false);
 	rq->nr_running--;
 }
+#ifdef CONFIG_CPU_QUIET
+#define NR_AVE_SCALE(x)		((x) << FSHIFT)
+static inline u64 do_nr_running_integral(struct rq *rq)
+{
+	s64 nr, deltax;
+	u64 nr_running_integral = rq->nr_running_integral;
+
+	deltax = rq->clock_task - rq->nr_last_stamp;
+	nr = NR_AVE_SCALE(rq->nr_running);
+
+	nr_running_integral += nr * deltax;
+
+	return nr_running_integral;
+}
+
+static inline void inc_nr_running(struct rq *rq)
+{
+	write_seqcount_begin(&rq->ave_seqcnt);
+	rq->nr_running_integral = do_nr_running_integral(rq);
+	rq->nr_last_stamp = rq->clock_task;
+	__inc_nr_running(rq);
+	write_seqcount_end(&rq->ave_seqcnt);
+}
+
+static inline void dec_nr_running(struct rq *rq)
+{
+	write_seqcount_begin(&rq->ave_seqcnt);
+	rq->nr_running_integral = do_nr_running_integral(rq);
+	rq->nr_last_stamp = rq->clock_task;
+	__dec_nr_running(rq);
+	write_seqcount_end(&rq->ave_seqcnt);
+}
+#else
+#define inc_nr_running __inc_nr_running
+#define dec_nr_running __dec_nr_running
+#endif
+
+static inline void rq_last_tick_reset(struct rq *rq)
+{
+#ifdef CONFIG_NO_HZ_FULL
+	rq->last_sched_tick = jiffies;
+#endif
+}
+
 
 extern void update_rq_clock(struct rq *rq);
 
