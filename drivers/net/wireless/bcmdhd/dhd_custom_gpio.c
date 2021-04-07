f3af88f03af5a4d1a6e4cc1feb7dc722f7de1406
Arian <arian.kulmer@web.de>
2021-04-07 16:26:34 +0200

    net: wireless: bcmdhd: Fix formatting
    Change-Id: I0d0da0d6e644bbb4def70a9643459c51bb119c9b

--- drivers/net/wireless/bcmdhd/dhd_custom_gpio.c
+++ drivers/net/wireless/bcmdhd/dhd_custom_gpio.c
@@ -1,13 +1,13 @@
 /*
 * Customer code to add GPIO control during WLAN start/stop
 * Copyright (C) 1999-2014, Broadcom Corporation
-* 
+*
 *      Unless you and Broadcom execute a separate written software license
 * agreement governing use of this software, this software is licensed to you
 * under the terms of the GNU General Public License version 2 (the "GPL"),
 * available at http://www.broadcom.com/licenses/GPLv2.php, with the
 * following added to such license:
-* 
+*
 *      As a special exception, the copyright holders of this software give you
 * permission to link this software with independent modules, and to copy and
 * distribute the resulting executable under terms of your choice, provided that
@@ -15,7 +15,7 @@
 * the license of that module.  An independent module is a module which is not
 * derived from this software.  The special exception does not apply to any
 * modifications of the software.
-* 
+*
 *      Notwithstanding the above, under no circumstances may you combine this
 * software in any way with any other Broadcom software provided under a license
 * other than the GPL, without Broadcom's express prior written consent.
@@ -40,7 +40,7 @@
 #if defined(CUSTOMER_HW2)
 
 
-#endif 
+#endif
 
 #if defined(OOB_INTR_ONLY)
 
@@ -96,12 +96,12 @@ int dhd_customer_oob_irq_map(void *adapter, unsigned long *irq_flags_ptr)
 	gpio_request(dhd_oob_gpio_num, "oob irq");
 	host_oob_irq = gpio_to_irq(dhd_oob_gpio_num);
 	gpio_direction_input(dhd_oob_gpio_num);
-#endif 
-#endif 
+#endif
+#endif
 
 	return (host_oob_irq);
 }
-#endif 
+#endif
 
 /* Customer function to control hw specific wlan gpios */
 int
